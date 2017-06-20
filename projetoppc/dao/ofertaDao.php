c1b8e2c9cc96880be94189e39981b59d04169056
guido <guido@Guido.sc.senac.br>
2017-06-20 18:36:16 -0300

    Comitando ofertaDao

---
+++ projetoppc/dao/ofertaDao.php
@@ -0,0 +1,82 @@
+<?php
+require_once 'c:\xampp\htdocs\projetoppc\factory\connectionFactory.php';
+function inserirOferta(int $ppccod, int $unicod, string $ofecont, string $ofevagasmat, string $ofevagasvesp, string $ofevagasnot, PDO &$conn = null): bool {
+	if (is_null ( $conn ))
+		$conn = conectarAoBanco ( "localhost", "dbdep", "root", "" );
+	$insercaooferta = $conn->prepare ( "insert into oferta (ppccod, unicod, ofecont, ofevagasmat, ofevagasvesp, ofevagasnot) values (:ppccod, :unicod, :ofecont, :ofevagasmat, :ofevagasvesp, :ofevagasnot)" );
+	$insercaooferta->bindParam ( ":ppccod", $ppccod );
+	$insercaooferta->bindParam ( ":unicod", $unicod );
+	$insercaooferta->bindParam ( ":ofecont", $ofecont );
+	$insercaooferta->bindParam ( ":ofevagasmat", $ofevagasmat );
+	$insercaooferta->bindParam ( ":ofevagasvesp", $ofevagasvesp );
+	$insercaooferta->bindParam ( ":ofevagasnot", $ofevagasnot );
+	$resultado = $insercaooferta->execute ();
+	desconectarDoBanco ( $conn );
+	return $resultado;
+}
+function atualizarOferta(int $ppccod, int $unicod, string $ofecont, string $ofevagasmat, string $ofevagasvesp, string $ofevagasnot, PDO &$conn = null): bool {
+	if (is_null ( $conn ))
+		$conn = conectarAoBanco ( "localhost", "dbdep", "root", "" );
+	$atualizacaooferta = $conn->prepare ( "update oferta set ofecont = :ofecont, ofevagasmat = :ofevagasmat, ofevagasvesp = :ofevagasvesp, ofevagasnot = :ofevagasnot where ppccod = :ppccod and unicod = :unicod" );
+	$atualizacaooferta->bindParam ( ":ofecont", $ofecont );
+	$atualizacaooferta->bindParam ( ":ofevagasmat", $ofevagasmat );
+	$atualizacaooferta->bindParam ( ":ofevagasvesp", $ofevagasvesp );
+	$atualizacaooferta->bindParam ( ":ofevagasnot", $ofevagasnot );
+	$atualizacaooferta->bindParam ( ":ppccod", $ppccod );
+	$atualizacaooferta->bindParam ( ":unicod", $unicod );
+	$resultado = $atualizacaooferta->execute ();
+	desconectarDoBanco ( $conn );
+	return $resultado;
+}
+function excluirOferta(int $ppccod, int $unicod): bool {
+	if (is_null ( $conn ))
+		$conn = conectarAoBanco ( "localhost", "dbdep", "root", "" );
+	$deloferta = $conn->prepare ( "delete from oferta where ppccod = :ppccod and unicod = :unicod" );
+	$deloferta->bindParam ( ":ppccod", $ppccod );
+	$deloferta->bindParam ( ":unicod", $unicod );
+	$resultado = $deloferta->execute ();
+	desconectarDoBanco ( $conn );
+	return $resultado;
+}
+function buscarOfertasPorPpc(int $ppccod, PDO &$conn = null): array {
+	$informacoesoferta = [ ];
+	if (is_null ( $conn ))
+		$conn = conectarAoBanco ( "localhost", "dbdep", "root", "" );
+	$consultaoferta = $conn->prepare ( "select * from oferta where ppccod = :ppccod" );
+	$consultaoferta->bindParam ( ":ppccod", $ppccod );
+	if (! $consultaoferta->execute ()) {
+		desconectarDoBanco ( $conn );
+		return $informacoesoferta;
+	} elseif ($consultaoferta->execute () && $consultaoferta->rowCount () == 0) {
+		desconectarDoBanco ( $conn );
+		return $informacoesoferta;
+	} elseif ($consultaoferta->execute () && $consultaoferta->rowCount () > 0) {
+		for($i = 0; $i < $consultaoferta->rowCount (); $i ++) {
+			$informacoesoferta [$i] = $consultaoferta->fetch ( PDO::FETCH_ASSOC );
+		}
+	}
+	desconectarDoBanco ( $conn );
+	return $informacoesoferta;
+}
+function buscarOfertasPorUnidade(int $unicod, PDO &$conn = null): array {
+	$informacoesoferta = [ ];
+	if (is_null ( $conn ))
+		$conn = conectarAoBanco ( "localhost", "dbdep", "root", "" );
+	$consultaoferta = $conn->prepare ( "select * from oferta where unicod = :unicod" );
+	$consultaoferta->bindParam ( ":unicod", $unicod );
+	if (! $consultaoferta->execute ()) {
+		desconectarDoBanco ( $conn );
+		return $informacoesoferta;
+	} elseif ($consultaoferta->execute () && $consultaoferta->rowCount () == 0) {
+		desconectarDoBanco ( $conn );
+		return $informacoesoferta;
+	} elseif ($consultaoferta->execute () && $consultaoferta->rowCount () > 0) {
+		for($i = 0; $i < $consultaoferta->rowCount (); $i ++) {
+			$informacoesoferta [$i] = $consultaoferta->fetch ( PDO::FETCH_ASSOC );
+		}
+	}
+	desconectarDoBanco ( $conn );
+	return $informacoesoferta;
+}
+
+?>
