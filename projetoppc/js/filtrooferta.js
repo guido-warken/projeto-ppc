4ab0a9a99c5186d80c5229b0b0a359a235c48406
guido <guido@Guido.sc.senac.br>
2017-08-15 13:11:07 -0300

    Comitando alterações no javascript

--- projetoppc/js/filtrooferta.js
+++ projetoppc/js/filtrooferta.js
@@ -4,8 +4,8 @@
 function gerenciarFiltro() {
 	var divPpc = document.getElementById("div-ppc");
 	var divUnidade = document.getElementById("div-unidade");
-	var opcao1 = document.getElementById("oferta-opt1");
-	var opcao2 = document.getElementById("oferta-opt2");
+	var opcao1 = document.getElementById("opt1");
+	var opcao2 = document.getElementById("opt2");
 	if (isChecked(opcao1)) {
 		divUnidade.hidden = true;
 		divPpc.hidden = false;
