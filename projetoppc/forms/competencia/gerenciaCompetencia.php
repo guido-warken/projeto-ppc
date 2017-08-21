b68111f9589dba1b2ba2ed7dc0893e998cdfe706
guido <guido@Guido.sc.senac.br>
2017-08-21 13:21:06 -0300

    Comitando novas alterações

--- projetoppc/forms/competencia/gerenciaCompetencia.php
+++ projetoppc/forms/competencia/gerenciaCompetencia.php
@@ -2,45 +2,31 @@
 require_once 'c:\wamp64\www\projetoppc\dao\competenciaDao.php';
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Gerenciamento de competências</title>
-<link rel="stylesheet"
-	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script
-	src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script
-	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-	<div class="container">
+<div class="container">
 	<?php
 if ($_GET["opcao"] == "cadastrar") :
     ?>
-		<h2>Cadastro de competências</h2>
+		<h2 class="text-center">Cadastro de competências</h2>
+	<br>
+	<form action="" method="post">
+		<div class="form-group">
+			<label for="compdes">Competência: </label>
+			<textarea rows="3" cols="3" id="compdes" name="compdes"
+				class="form-control"></textarea>
+		</div>
 		<br>
-		<form action="" method="post">
-			<div class="form-group">
-				<label for="compdes">Competência: </label>
-				<textarea rows="3" cols="3" id="compdes" name="compdes"
-					class="form-control"></textarea>
-			</div>
-			<br>
-			<div class="form-group">
-				<input type="submit" class="btn btn-success" value="salvar">
-			</div>
-			<br>
-		</form>
+		<div class="form-group">
+			<input type="submit" class="btn btn-default" value="salvar">
+		</div>
+		<br>
+	</form>
 	<?php
     if (! array_key_exists("compdes", $_POST))
         return;
     try {
         if (inserirCompetencia($_POST["compdes"])) {
-            echo "<h1>Competência cadastrada com êxito!</h1><br>";
-            echo "<a href= 'gerenciaCompetencia.php?opcao=consultar'>Clique aqui para consultar as competências cadastradas</a><br>";
+            echo "<h1 class= 'text-success'>Competência cadastrada com êxito!</h1><br>";
+            echo "<a href= '?pagina=competencia&opcao=consultar'>Clique aqui para consultar as competências cadastradas</a><br>";
         }
     } catch (PDOException $e) {
         echo $e->getMessage();
@@ -48,74 +34,73 @@ if ($_GET["opcao"] == "cadastrar") :
  elseif ($_GET["opcao"] == "consultar") :
     $competencias = buscarCompetencias();
     ?>
-		<h2>Consulta de competências</h2>
-		<br> <a href="gerenciaCompetencia.php?opcao=cadastrar">Nova
-			competência</a><br>
+		<h2 class="text-center">Consulta de competências</h2>
+	<br> <a href="?pagina=competencia&opcao=cadastrar">Nova competência</a><br>
 		<?php
     if (count($competencias) > 0) :
         ?>
 		<h2>Número de competências encontradas: <?=count($competencias); ?></h2>
-		<br>
-		<table class="table table-bordered">
-			<thead>
-				<tr>
-					<th>Competência</th>
-					<th colspan="2">Ação</th>
-				</tr>
-			</thead>
-			<tbody>
+	<br>
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<th>Competência</th>
+				<th colspan="2">Ação</th>
+			</tr>
+		</thead>
+		<tbody>
 		<?php
         foreach ($competencias as $competencia) :
             ?>
 				<tr>
-					<td><?=$competencia["compdes"]; ?></td>
-					<td><a
-						href="gerenciaCompetencia.php?opcao=alterar&compcod=<?=$competencia['compcod']; ?>">alterar
-							dados</a></td>
-					<td><a
-						href="gerenciaCompetencia.php?opcao=excluir&compcod=<?=$competencia['compcod']; ?>">excluir</a>
-					</td>
-				</tr>
+				<td><?=$competencia["compdes"]; ?></td>
+				<td><a
+					href="?pagina=competencia&opcao=alterar&compcod=<?=$competencia['compcod']; ?>">alterar
+						dados</a></td>
+				<td><a
+					href="?pagina=competencia&opcao=excluir&compcod=<?=$competencia['compcod']; ?>">excluir</a>
+				</td>
+			</tr>
 		<?php
         endforeach
         ;
         ?>
 		</tbody>
-		</table>
+	</table>
 		<?php
     else :
         ?>
-		<h2>Nenhuma competência cadastrada</h2>
-		<br> <a href="gerenciaCompetencia.php?opcao=cadastrar">Cadastrar uma
-			nova competência</a><br>
+		<h2 class="text-warning">Nenhuma competência cadastrada</h2>
+	<br> <a href="?pagina=competencia&opcao=cadastrar">Cadastrar uma nova
+		competência</a><br>
 		<?php
     endif;
  elseif ($_GET["opcao"] == "alterar") :
     $competencia = buscarCompetenciaPorId($_GET["compcod"]);
     ?>
-	<h2>Alteração de competência</h2>
-		<br>
-		<form action="" method="post">
-			<div class="form-group">
-				<label for="compdes">Competência: </label>
-				<textarea rows="3" cols="3" id="compdes" name="compdes"
-					class="form-control">
+	<h2 class="text-center">Alteração de competência</h2>
+	<br>
+	<form action="" method="post">
+		<div class="form-group">
+			<label for="compdes">Competência: </label>
+			<textarea rows="3" cols="3" id="compdes" name="compdes"
+				class="form-control">
 					<?=$competencia["compdes"]; ?>
 					</textarea>
-			</div>
-			<br>
-			<div class="form-group">
-				<input type="submit" class="btn btn-success" value="alterar">
-			</div>
-			<br>
-		</form>
+		</div>
+		<br>
+		<div class="form-group">
+			<input type="submit" class="btn btn-default" value="alterar">
+		</div>
+		<br>
+	</form>
 		<?php
     if (! array_key_exists("compdes", $_POST))
         return;
     try {
         if (atualizarCompetencia($_POST["compdes"], $competencia["compcod"])) {
-            echo "<h1>Competência atualizada com êxito!</h1><br>";
-            echo "<a href= 'gerenciaCompetencia.php?opcao=consultar'>Clique aqui para consultar novamente as competências</a><br>";
+            echo "<h1 class= 'text-success'>Competência atualizada com êxito!</h1><br>";
+            echo "<a href= '?pagina=competencia&opcao=consultar'>Clique aqui para consultar novamente as competências</a><br>";
         }
     } catch (PDOException $e) {
         echo $e->getMessage();
@@ -124,27 +109,27 @@ if ($_GET["opcao"] == "cadastrar") :
     $competencia = buscarCompetenciaPorId($_GET["compcod"]);
     ?>
 	<h2>Exclusão de competência</h2>
-		<br>
-		<form action="" method="post">
-			<div class="form-group">
-				<p>
+	<br>
+	<form action="" method="post">
+		<div class="form-group">
+			<p>
 	Você está prestes a excluir a competência <?=$competencia["compdes"]; ?>. <br>Tem
-					certeza de que deseja executar esta operação?<br> Após a
-					confirmação, esta operação não poderá ser desfeita.
-				</p>
-			</div>
-			<br>
-			<div class="form-group">
-				<input type="submit" value="sim" name="escolha"
-					class="btn btn-success">
-			</div>
-			<br>
-			<div class="form-group">
-				<input type="submit" value="não" name="escolha"
-					class="btn btn-success">
-			</div>
-			<br>
-		</form>
+				certeza de que deseja executar esta operação?<br> Após a
+				confirmação, esta operação não poderá ser desfeita.
+			</p>
+		</div>
+		<br>
+		<div class="form-group">
+			<input type="submit" value="sim" name="escolha"
+				class="btn btn-success">
+		</div>
+		<br>
+		<div class="form-group">
+			<input type="submit" value="não" name="escolha"
+				class="btn btn-success">
+		</div>
+		<br>
+	</form>
 	<?php
     if (! array_key_exists("escolha", $_POST))
         return;
@@ -163,5 +148,3 @@ if ($_GET["opcao"] == "cadastrar") :
 endif;
 ?>
 	</div>
-</body>
-</html>
